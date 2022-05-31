743d4e0f79eb60776dbc5784393367c241cfcefd
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-05-31 02:26:33 -0400

    build a mongoose book schema

--- server.js
+++ server.js
@@ -3,9 +3,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
+const schema = require('./Modules/seed');
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
 
 const PORT = process.env.PORT || 3001;
 
