547ae886ec972c7bbbc6c8f39ea5d7292e04a00b
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-06-07 21:12:36 -0400

    added update feature

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,45 +1,59 @@
 'use strict';
 
 
- const BookModel = require('../models/book');
+const BookModel = require('../models/book');
 
 const Handlers = {};
 
- Handlers.getBooks = async ( request , response, next) => {
-   try{
-     const books = await BookModel.find({});
-     response.status(200).send(books);
-    
-   }catch(error) {
-     console.error(error);
-     next(error);
-   }
- }
+Handlers.getBooks = async (request, response, next) => {
+  try {
+    const books = await BookModel.find({});
+    response.status(200).send(books);
 
- Handlers.createBook = async ( request, response, next) => {
-   try{
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+Handlers.createBook = async (request, response, next) => {
+  try {
     const newBook = await BookModel.create(request.body);
     response.status(201).send(newBook);
 
-   }catch(error){
-    console.error(error);
+  } catch (error) {
+    error.customMessage = 'something went wrong when creating a book: ';
+    console.error(error.customMessage + error);
     next(error);
-   }
- }
+  }
+}
 
- Handlers.deleteBook = async ( request, response, next) => {
-  try{
- await BookModel.findByIdAndDelete(request.params.id);
-   response.status(204);
+Handlers.deleteBook = async (request, response, next) => {
+  try {
+    await BookModel.findByIdAndDelete(request.params.id);
+    response.status(204);
 
-  }catch(error){
-    error.customMessage = 'something went wrong when deleting a book:';
-   console.error(error.customMessage + error);
-   next(error);
+  } catch (error) {
+    error.customMessage = 'something went wrong when deleting a book: ';
+    console.error(error.customMessage + error);
+    next(error);
+  }
+}
+
+
+Handlers.updateBook = async (request, response, next) => {
+  try {
+    const updatedBook = await BookModel.findByIdAndUpdate(request.params.id, request.body, { new: true });
+    response.status(200).send(updatedBook);
+
+  } catch (error) {
+    error.customMessage = 'something went wrong when updating a book: ';
+    console.error(error.customMessage + error);
+    next(error);
   }
 }
 
 
 
 
- module.exports = Handlers;
+module.exports = Handlers;
