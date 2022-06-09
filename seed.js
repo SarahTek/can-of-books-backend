4a49b46db0d4b4e379baf99b9360499288e9609f
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-06-09 18:49:12 -0400

    working Auth0

--- seed.js
+++ seed.js
@@ -14,19 +14,21 @@ async function seedBooks() {
     title: 'How Far You Have Come',
     description: 'exquisite collection of poetry and essays from best-selling artist and writer Morgan Harper Nichols. In the midst of the hurt and the mundane, the questions and the not yets, you can forget just how far you have come. Morgan weaves together personal reflections with her signature poems, encouraging you to reclaim moments of brokenness, division, and pain and re-envision them as experiences of reconciliation, unity, and hope',
     status: 'FAVOURITE FIVE',
-    
+    email: 'sarah.f.tsion@gmail.com'
   });
 
   await BookModel.create({
     title: 'You Can Heal Your Life',
     description: 'how limiting beliefs and ideas are often the cause of illness, and how you can change your thinking…and improve the quality of your life.',
-    status: 'LIFE-CHANGING'
+    status: 'LIFE-CHANGING',
+    email: 'sarah.f.tsion@gmail.com'
   });
 
   await BookModel.create({
     title: 'How To Be Alone',
     description: 'A must-read for anyone whose childhood still feels unresolved, who spends more time pretending to have friends online than feeling close to anyone in real life, who tries to have genuine, deep conversations in a roomful of people who would rather you not. Above all, it is an audiobook for anyone who desperately wants to feel less alone and a little more connected through listening to her words. ',
-    status: 'FAVOURITE FIVE'
+    status: 'FAVOURITE FIVE',
+    email: 'sarah.f.tsion@gmail.com'
   });
 
   mongoose.disconnect();
