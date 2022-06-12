ea91682cfe2c72200e5783c83ffc152dbafe7e10
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-06-12 18:17:33 -0400

    finished Lab15

--- models/book.js
+++ models/book.js
@@ -5,7 +5,7 @@ const bookSchema = new mongoose.Schema({
   title: {type: String, required: true},
   description: {type: String, required: true},
   status: {type: String, uppercase: true, enum: ['LIFE-CHANGING', 'FAVOURITE FIVE', 'RECOMMENDED TO ME', 'POETRY', 'ROMANTIC NOVEL'] },
-   email: {type: String, required: true}
+  email: {type: String, required: true}
 });
 
 
